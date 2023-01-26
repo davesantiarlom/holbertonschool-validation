deb29688bbd64d266b2d59f127506319f0979304
davesantiarlom <david.arias02@usc.edu.co>
2023-01-26 17:17:22 -0500

    Tasks

--- module2_task5/setup.sh
+++ module2_task5/setup.sh
@@ -1,15 +1,6 @@
-# Update and install 'hugo' and 'make'
-apt-get update && apt-get install hugo make
-
-# Install 'curl'
-apt-get install curl -y
-
-# Download latest release of 'hugo'
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-
-# Install the latest version of 'hugo'
-apt-get install ./install_hugo.deb
-rm ./install_hugo.deb
-
-# Build the website
+#!/usr/bin/env bash
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+rm -f hugo_extended_0.109.0_linux-amd64.deb
 make build
